120acae21fdb75dfd3f78deea72a17f3edccd91e
jlclx <46258217+jlclx@users.noreply.github.com>
2021-10-03 06:10:24 -0500

    Modify webhook name.

--- deployments/runtimeclass-controller.sh
+++ deployments/runtimeclass-controller.sh
@@ -2,7 +2,7 @@ tmp=$(mktemp -d -t runtimeclass-controller-certs-XXXXXXXXXX)
 self="$(dirname "$(realpath "$0")")"
 openssl req -nodes -new -x509 -keyout $tmp/ca.key -out $tmp/ca.crt -subj "/CN=RuntimeClass Controller" 2>/dev/null
 openssl genrsa -out $tmp/tls.key 2048 2>/dev/null
-openssl req -new -key $tmp/tls.key -subj "/CN=runtimeclass-controller.default.svc" 2>/dev/null | \
+openssl req -new -key $tmp/tls.key -subj "/CN=runtimeclass-controller.runtimeclass-controller.svc" 2>/dev/null | \
 openssl x509 -req -CA $tmp/ca.crt -CAkey $tmp/ca.key -CAcreateserial -out $tmp/tls.crt 2>/dev/null
 CA_BUNDLE=$(cat $tmp/ca.crt | base64 | tr -d '\n')
 kubectl --namespace runtimeclass-controller create secret tls runtimeclass-controller-certs \
