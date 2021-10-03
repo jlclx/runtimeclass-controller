a012f396789c8addbf2080f454f26c2259a05696
jlclx <46258217+jlclx@users.noreply.github.com>
2021-10-03 05:27:43 -0500

    Initial

---
+++ cmd/runtimeclass-controller/main.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	admission "k8s.io/api/admission/v1"
+	core "k8s.io/api/core/v1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	log "k8s.io/klog/v2"
+	"net/http"
+)
+
+type controller struct {
+	 deserializer runtime.Decoder
+	 client *kubernetes.Clientset
+}
+
+type PatchResult struct {
+	Allowed bool
+	Message string
+	Patches []Patch
+}
+
+type Patch struct {
+	Operation string `json:"op"`
+	Path      string `json:"path"`
+	From  string      `json:"from"`
+	Value interface{} `json:"value,omitempty"`
+}
+
+func main() {
+
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	c := controller{
+		deserializer: serializer.NewCodecFactory(runtime.NewScheme()).UniversalDeserializer(),
+		client: clientset,
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", c.health)
+	mux.HandleFunc("/mutate", c.Serve)
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":8080"),
+		Handler: mux,
+	}
+
+	if err := server.ListenAndServeTLS("/certs/tls.crt", "/certs/tls.key"); err != nil {
+		log.Errorf("Failed to listen and serve: %v", err)
+	}
+}
+
+
+func (c *controller) Serve(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method != http.MethodPost {
+		http.Error(w, fmt.Sprint("POST only"), http.StatusMethodNotAllowed)
+		return
+	}
+
+	if contentType := r.Header.Get("Content-Type"); contentType != "application/json" {
+		http.Error(w, fmt.Sprint("application/json content only"), http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("bad request body"), http.StatusBadRequest)
+		return
+	}
+
+	var review admission.AdmissionReview
+	if _, _, err := c.deserializer.Decode(body, nil, &review); err != nil {
+		http.Error(w, fmt.Sprintf("failed to decode request: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	if review.Request == nil {
+		http.Error(w, "bad admission review", http.StatusBadRequest)
+		return
+	}
+
+	result, err := c.Patch(review.Request)
+	if err != nil {
+		log.Error(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	response := admission.AdmissionReview{
+		Response: &admission.AdmissionResponse{
+			UID:     review.Request.UID,
+			Allowed: result.Allowed,
+			Result:  &meta.Status{Message: result.Message},
+		},
+	}
+
+	if len(result.Patches) > 0 {
+		patches, err := json.Marshal(result.Patches)
+		if err != nil {
+			log.Error(err)
+			http.Error(w, fmt.Sprintf("could not serialize JSON patch: %v", err), http.StatusInternalServerError)
+		}
+		response.Response.Patch = patches
+	}
+
+
+	responseJson, err := json.Marshal(response)
+	if err != nil {
+		log.Error(err)
+		http.Error(w, fmt.Sprintf("could not serialize admission response: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	log.Infof("Webhook [%s - %s] - Allowed: %t", r.URL.Path, review.Request.Operation, result.Allowed)
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(responseJson)
+}
+
+func (c *controller) Patch(r *admission.AdmissionRequest) (*PatchResult, error) {
+	var patches []Patch
+	var pod core.Pod
+
+	if err := json.Unmarshal(r.Object.Raw, &pod); err != nil {
+		return &PatchResult{
+			Message: err.Error(),
+		}, nil
+	}
+
+	//if pod.Namespace {
+	if pod.Spec.RuntimeClassName == nil {
+		namespace, err:= c.client.CoreV1().Namespaces().Get(context.TODO(), pod.Namespace, meta.GetOptions{})
+		if err != nil {
+			return &PatchResult{
+				Message: err.Error(),
+			}, nil
+		}
+
+		if val, ok := namespace.Labels["runtimeclassname-default"]; ok {
+			patches = append(patches, Patch{
+				Operation: "add",
+				Path:      "/spec/runtimeClassName",
+				Value:     val,
+			})
+
+			return &PatchResult{
+				Allowed: true,
+				Patches: patches,
+			}, nil
+		}
+	}
+
+	return &PatchResult{
+		Allowed: true,
+	}, nil
+}
+
+func (c *controller) health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("ok"))
+}
---
+++ deployments/runtimeclass-controller.sh
@@ -0,0 +1,15 @@
+tmp=$(mktemp -d -t runtimeclass-controller-certs-XXXXXXXXXX)
+self="$(dirname "$(realpath "$0")")"
+openssl req -nodes -new -x509 -keyout $tmp/ca.key -out $tmp/ca.crt -subj "/CN=RuntimeClass Controller" 2>/dev/null
+openssl genrsa -out $tmp/tls.key 2048 2>/dev/null
+openssl req -new -key $tmp/tls.key -subj "/CN=runtimeclass-controller.default.svc" 2>/dev/null | \
+openssl x509 -req -CA $tmp/ca.crt -CAkey $tmp/ca.key -CAcreateserial -out $tmp/tls.crt 2>/dev/null
+CA_BUNDLE=$(cat $tmp/ca.crt | base64 | tr -d '\n')
+kubectl create secret tls admission-tls \
+    --cert "$tmp/tls.crt" \
+    --key "$tmp/tls.key" \
+    -o yaml --dry-run=client
+echo "---"
+sed -e 's@${CA_BUNDLE}@'"$CA_BUNDLE"'@g' <"$self/runtimeclass-controller.yaml"
+
+rm -rf $tmp
