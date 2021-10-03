6994190880e5ab2536dfc001a20feb3874975726
jlclx <46258217+jlclx@users.noreply.github.com>
2021-10-03 06:40:14 -0500

    Hopefully generates correct certs.

--- deployments/runtimeclass-controller.sh
+++ deployments/runtimeclass-controller.sh
@@ -1,15 +1,14 @@
 tmp=$(mktemp -d -t runtimeclass-controller-certs-XXXXXXXXXX)
 self="$(dirname "$(realpath "$0")")"
-openssl req -nodes -new -x509 -keyout $tmp/ca.key -out $tmp/ca.crt -subj "/CN=RuntimeClass Controller" 2>/dev/null
-openssl genrsa -out $tmp/tls.key 2048 2>/dev/null
-openssl req -new -key $tmp/tls.key -subj "/CN=runtimeclass-controller.runtimeclass-controller.svc" 2>/dev/null | \
-openssl x509 -req -CA $tmp/ca.crt -CAkey $tmp/ca.key -CAcreateserial -out $tmp/tls.crt 2>/dev/null
-CA_BUNDLE=$(cat $tmp/ca.crt | base64 | tr -d '\n')
+openssl req -nodes -new -x509 -keyout $tmp/ca-key.pem -out $tmp/ca-crt.pem -subj "/CN=runtimeclass-controller.runtimeclass-controller.svc" 2>/dev/null
+openssl genrsa -out $tmp/key.pem 2048 2>/dev/null
+openssl req -new -key $tmp/key.pem -subj "/CN=runtimeclass-controller.runtimeclass-controller.svc" 2>/dev/null | \
+openssl x509 -req -CA $tmp/ca-crt.pem -CAkey $tmp/ca-key.pem -CAcreateserial -out $tmp/crt.pem 2>/dev/null
+CA_BUNDLE=$(cat $tmp/ca-crt.pem | base64 | tr -d '\n')
 kubectl --namespace runtimeclass-controller create secret tls runtimeclass-controller-certs \
-    --cert "$tmp/tls.crt" \
-    --key "$tmp/tls.key" \
+    --cert "$tmp/crt.pem" \
+    --key "$tmp/key.pem" \
     -o yaml --dry-run=client
 echo "---"
 sed -e 's@${CA_BUNDLE}@'"$CA_BUNDLE"'@g' <"$self/runtimeclass-controller.yaml"
-
 rm -rf $tmp
